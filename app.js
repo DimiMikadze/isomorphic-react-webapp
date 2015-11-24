a23aa7b4a47d9a6946cd42106d6f6f2e03ee0e4d
Dimitri Mikadze <dimitrimikadze@gmail.com>
2015-11-24 23:15:31 +0400

    update server

--- app.js
+++ app.js
@@ -6,17 +6,6 @@ app.set('view engine', 'html');
 app.use(express.static("public"));
 app.use(express.static("views"));
 
-function wwwRedirect(req, res, next) {
-    if (req.headers.host.slice(0, 4) === 'www.') {
-        var newHost = req.headers.host.slice(4);
-        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-    }
-    next();
-}
-
-app.set('trust proxy', true);
-app.use(wwwRedirect);
-
 app.get('*', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
