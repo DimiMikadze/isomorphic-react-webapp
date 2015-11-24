2baeae776a70e0c4321fac806b74f639422abeac
Dimitri Mikadze <dimitrimikadze@gmail.com>
2015-11-24 22:34:30 +0400

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var env = require('gulp-env');
 
 gulp.task('sass', function () {
     gulp.src('./public/sass/bundle.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('set-env', function () {
-    env({
-        vars: {
-            PORT: 80
-        }
-    })
 });
 
 gulp.task('sass:watch', function () {
