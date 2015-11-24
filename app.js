24f322d8152444eadd670c033f6fbd01b2954f0d
Dimitri Mikadze <dimitrimikadze@gmail.com>
2015-11-24 22:01:04 +0400

    change port

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.get('*', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+var port = process.env.PORT || '3000';
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(port);
