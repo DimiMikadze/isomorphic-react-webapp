073efff063894b0563e029d6aa602593a5e98d4d
Dimitri Mikadze <dimitrimikadze@gmail.com>
2015-12-02 22:57:20 +0400

    remove angular

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static("public"));
 app.use(express.static("views"));
 
+app.get('/', function(req, res) {
+    res.render(__dirname + '/views/me', { title: '' });
+});
+
+app.get('/work', function(req, res) {
+    res.render(__dirname + '/views/work', { title: ' - Work' });
+});
+
+app.get('/skills', function(req, res) {
+    res.render(__dirname + '/views/skills', { title : ' - Skills' });
+});
+
 app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+    res.redirect('/');
 });
 
 var port = process.env.PORT || '3000';
