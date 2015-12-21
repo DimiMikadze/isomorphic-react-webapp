29d2c59469e6e879d0ded8791b9341e44fe03de5
Dimitri Mikadze <dimitrimikadze@gmail.com>
2015-12-22 00:07:53 +0400

    redesign

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 app.use(express.static("views"));
 
 app.get('/', function(req, res) {
-    res.render(__dirname + '/views/me', { title: '' });
+    res.render(__dirname + '/views/me', { title: 'Me' });
 });
 
 app.get('/work', function(req, res) {
