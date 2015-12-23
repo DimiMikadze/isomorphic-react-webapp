5835418e6e45f42f0a4b44a128738f58313f78ae
Dimitri Mikadze <dimitrimikadze@gmail.com>
2015-12-23 15:09:32 +0400

    minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
 
 gulp.task('sass', function () {
     gulp.src('./public/sass/bundle.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('compress', function() {
+    return gulp.src('./public/js/bundle.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js/'));
 });
 
 gulp.task('sass:watch', function () {
