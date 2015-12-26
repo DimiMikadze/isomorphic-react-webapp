7b4703deffe4d3b93a136e46061cc54b10e255c2
Dimitri Mikadze <dimitrimikadze@gmail.com>
2015-12-26 17:56:44 +0400

    major refactor

--- app/components/Work.js
+++ app/components/Work.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import Slider from './Slider';
+import SlickSlider from 'react-slick';
+import { Link } from 'react-router';
+import projects from '../utils/projects';
 
 class Work extends React.Component {
 
     render() {
+
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            draggable: true
+        };
+
+        let images = projects.map((p, index) => {
+            return (
+                <Link to={`/project/${p.name}`} >
+                    <div className="slider-item" key={index}
+                         style={{backgroundImage: 'url(../' + p.main_image + ')',
+                         backgroundSize: 'cover',
+                         backgroundPosition: '50%' }}
+                    >
+                    </div>
+                </Link>
+            )
+        });
+
         return (
             <div className="work-container">
-                <Slider />
+                <SlickSlider {...settings}>
+                    {images}
+                </SlickSlider>
             </div>
         )
     }
