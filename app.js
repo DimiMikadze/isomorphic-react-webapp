42cba5884a5f03fa6cf99b241685f96cc831b31a
Dimitri Mikadze <dimitrimikadze@gmail.com>
2016-06-18 20:07:02 +0400

    add geegle

--- app.js
+++ app.js
@@ -15,33 +15,36 @@ app.use(express.static("static"));
 app.use(express.static("dist"));
 
 app.get('/sitemap.xml', (req, res) => {
-    res.header('Content-Type', 'application/xml');
-    res.render('sitemap');
+  res.header('Content-Type', 'application/xml');
+  res.render('sitemap');
 });
 
 app.get('*', (req, res) => {
-    match({ routes, location: req.url }, (err, redirectLocation, props) => {
+  match({routes, location: req.url}, (err, redirectLocation, props) => {
 
-        if (req.headers.host.match(/^www/) !== null ) {
-            res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
-        } else if (err) {
-            res.status(500).send(err.message);
-        } else if (redirectLocation) {
-            res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-        } else if (props) {
-            const content = renderToString(<RoutingContext {...props} />);
-            res.render('index', { content })
-        } else {
-            res.sendStatus(404);
-        }
-    });
+    console.log('REQ URL' + req.url);
+    console.log('HEADER HOST' + req.headers.host);
+
+    if (req.headers.host.match(/^www/) !== null) {
+      res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+    } else if (err) {
+      res.status(500).send(err.message);
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    } else if (props) {
+      const content = renderToString(<RoutingContext {...props} />);
+      res.render('index', {content})
+    } else {
+      res.sendStatus(404);
+    }
+  });
 });
 
 let port = process.env.PORT || '3000';
 
-const server = app.listen(port,()  => {
-    const host = server.address().address;
-    const port = server.address().port;
+const server = app.listen(port, ()  => {
+  const host = server.address().address;
+  const port = server.address().port;
 
-    console.log("app listening on " + host + " " + port);
+  console.log("app listening on " + host + " " + port);
 });
